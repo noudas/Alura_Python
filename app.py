2ab50e00c574a78965a2b1492705687f472a448f
Jonathan K. <noudas1@gmail.com>
2024-08-21 22:16:25 -0300

    dia 2

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print("""
+import os
+
+def exibir_nome_do_programa():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -7,10 +10,34 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 \n""")
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Voce escolheu: {opcao_escolhida}')
+def finalizar_app():
+    os.system('cls')
+    print('Encerrando o programa\n')
+
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print(f'Voce escolheu: {opcao_escolhida}')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurantes')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+
+if __name__ == '__main__':
+    main()
--- exercicios.py
+++ exercicios.py
@@ -36,3 +36,67 @@ def exercicio4():
     pi_arredondado = round(pi,2)
     print(f'O valor arredondado de pi é: {pi_arredondado}')
 
+
+
+# Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar.
+def exercicio5():
+    numero = int(input("insira um número: "))
+    if numero % 2 == 0:
+        print("Numero Par")
+    else:
+        print("Numero Impar")
+
+
+
+"""
+Pergunte ao usuário sua idade e, com base nisso, use uma estrutura if elif else para classificar a idade em categorias de acordo com as seguintes condições:
+
+Criança: 0 a 12 anos;
+Adolescente: 13 a 18 anos;
+Adulto: acima de 18 anos.
+
+"""
+
+def exercicio6():
+    idade = int(input('Digite a sua idade: '))
+    if 0 < idade < 12:
+        print("Criança")
+    elif 13 < idade < 18:
+        print("Adolescente")
+    else:
+        print("Adulto")
+        
+def exercicio6v2():
+    idade = int(input('Digite a sua idade: '))
+    match idade:
+        case i if 0 < i < 12:
+            print("Criança")
+        case i if 13 < i < 18:
+            print("Adolescente")
+        case i if i > 18:
+            print("Adulto")
+
+
+"""
+Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o 
+nome de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.
+"""
+def exercicio7():
+    usuario = input('Digite o usuario: ')
+    senha = input('Digite a senha: ')
+
+    if usuario == 'Lumbert' and senha == 'senha123':
+        print('Sucesso')
+    else:
+        print('Errou')
+
+"""
+Solicite ao usuário as coordenadas (x, y) de um ponto qualquer e utilize uma estrutura if elif else para determinar em qual quadrante do plano cartesiano o ponto se encontra de acordo com as seguintes condições:
+
+Primeiro Quadrante: os valores de x e y devem ser maiores que zero;
+Segundo Quadrante: o valor de x é menor que zero e o valor de y é maior que zero;
+Terceiro Quadrante: os valores de x e y devem ser menores que zero;
+Quarto Quadrante: o valor de x é maior que zero e o valor de y é menor que zero;
+Caso contrário: o ponto está localizado no eixo ou origem.
+"""
+
